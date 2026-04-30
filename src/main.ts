25336f51e1379a68ca59deee0b215a98684c2e00
1172004a <truonggiang11724@gmail.com>
2026-04-30 23:23:04 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // React
+    origin: '*', // React
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
